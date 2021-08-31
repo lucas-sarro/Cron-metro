091f43d103baac99fe8dc2b22761f470a926fc61
Lucas Sarro <sarro.lucas85@gmail.com>
2021-08-31 16:43:19 -0300

    Criado uma parte do Java Script, mas com bug

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+let visor = document.getElementById('visor')
  
+
+function contar(){
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let mili = date.getMilliseconds();
+
+    visor.innerText = h+":"+m+":"+s+":"+mili;
+}
+
+function comecar(){
+    
+    timer = setInterval(contar, 10);
+}
+
+function parar(){
+    clearInterval(timer);
+}
+
+function zerar(){
+    parar();
+    visor.innerText = "00:00";
+}
+
